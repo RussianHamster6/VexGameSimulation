b1ab5870f3ff4215a49dd67a9b4de7fccf03937a
Declan Rhodes <declanrhodesss@gmail.com>
2019-02-08 10:07:08 +0000

    Created the classes including properties and methods for the Actions and GameObjects

---
+++ vexGameSimulation/vexGameSimulation/Action.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vexGameSimulation
+{
+    class Action
+    {
+        //Creating the auto implemented properties for the variables stated below so that they can be created by the constructor function that will be made after these.
+
+        public string actionName { get; set; } 
+        public float timeToComplete { get; set; }
+        public float probOfSuccess { get; set; }
+        public bool canDoAction { get; set; }
+
+        //Here is the constructor so that I can create actions with this template
+        public Action(string name, float time, float prob, bool canDoAc)
+        {
+            actionName = name; //Assigns name value from function to the actionName of the object
+            timeToComplete = time; //Assigns time value from function to the timeToComplete of the object 
+            probOfSuccess = prob; //Assigns prob value from function to the probOfSuccess of the object
+            canDoAction = canDoAc; //Assigns canDoAc from function to the canDoAction of the object
+        }
+
+        //Assigns the getter functions so that I can get the properties of an object from the object later
+        public string GetActionName() { return actionName; } 
+        public float GetTimeToComplete() { return timeToComplete; }
+        public float GetProbOfSuccess() { return probOfSuccess; }
+        public bool GetCanDoAction() { return canDoAction; }
+    }
+}
+
+
---
+++ vexGameSimulation/vexGameSimulation/GameObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vexGameSimulation
+{
+    class GameObject
+    {
+        //Creates the auto implemented properties for the below properties so that they can be later created in a constructor in the template
+        public string requiredAction { get; set; }
+        public float Xlocation { get; set; }
+        public float Ylocation { get; set; }
+        public int pointVal { get; set; }
+        public bool beenScored { get; set; }
+
+        //Constructor function to create a new GameObject
+        public GameObject(string reqAction, float X, float Y, int points, bool scored)
+        {
+            requiredAction = reqAction;
+            Xlocation = X;
+            Ylocation = Y;
+            pointVal = points;
+            beenScored = scored;
+        }
+
+        //Getter functions to return the specified property of the GameObject
+        public string GetRequiredAction() { return requiredAction; }
+        public float GetXlocation() { return Xlocation; }
+        public float GetYlocation() { return Ylocation; }
+        public int GetPointVal() { return pointVal; }
+        public bool GetBeenScored() { return beenScored; }
+    }
+}
+
+
